4df42b0e414f1cce9a6685c8967dfd497adccd90
Alessio <alessio@freedamedia.com>
2018-12-24 00:24:54 +0100

    Let user interface service handle result comunication

--- src/main/scala/com/galales/tictactoe/ui/UserInterfaceDrawer.scala
+++ src/main/scala/com/galales/tictactoe/ui/UserInterfaceDrawer.scala
@@ -1,8 +1,11 @@
 package com.galales.tictactoe.ui
 
+import com.galales.tictactoe.enums.GameResult
 import com.galales.tictactoe.models.Board
 
 trait UserInterfaceDrawer {
 
   def printBoard(board: Board)
+
+  def printResult(result: GameResult.Value): Unit = println(s"Result: $result")
 }
