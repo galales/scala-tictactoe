1195bc44c1c660ed19722438aa3df0e8a66c6de5
Alessio <alessio@freedamedia.com>
2018-12-23 22:56:48 +0100

    User input draft

--- src/main/scala/com/galales/tictactoe/interaction/ConsoleInteraction.scala
+++ src/main/scala/com/galales/tictactoe/interaction/ConsoleInteraction.scala
@@ -3,5 +3,8 @@ package com.galales.tictactoe.interaction
 import com.galales.tictactoe.models.Move
 
 object ConsoleInteraction extends UserInteractionHandler {
-  override def makeMove: Move = ???
+  override def makeMove: Move = {
+    println("Make your move (row column): ")
+    Move(scala.io.StdIn.readInt, scala.io.StdIn.readInt)
+  }
 }
