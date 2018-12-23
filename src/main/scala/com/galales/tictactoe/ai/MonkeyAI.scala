756371e0b69a2457f23f5cb647f23c33fabae4a2
Alessio <alessio@freedamedia.com>
2018-12-23 23:16:46 +0100

    Monkey move implementation

--- src/main/scala/com/galales/tictactoe/ai/MonkeyAI.scala
+++ src/main/scala/com/galales/tictactoe/ai/MonkeyAI.scala
@@ -4,6 +4,19 @@ import com.galales.tictactoe.models.{Board, Move}
 
 object MonkeyAI extends AI {
 
-  override def makeMove(board: Board) : Move = ???
+  private val random = scala.util.Random
+
+  // Set mark in a random spot
+  override def makeMove(board: Board) : Move = {
+    val emptyBoxes = for(
+      row <- 0 to 2;
+      column <- 0 to 2
+      if board(row)(column).isEmpty
+    ) yield (row, column)
+
+    val nextMove = emptyBoxes(random.nextInt(emptyBoxes.length))
+
+    Move(nextMove._1, nextMove._2)
+  }
 
 }
