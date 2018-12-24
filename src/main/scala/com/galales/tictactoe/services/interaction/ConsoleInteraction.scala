dcb59e3ee010df45478de05cd2dd768fc1cbbe43
Alessio <alessio@freedamedia.com>
2018-12-24 11:55:02 +0100

    Input check

--- src/main/scala/com/galales/tictactoe/services/interaction/ConsoleInteraction.scala
+++ src/main/scala/com/galales/tictactoe/services/interaction/ConsoleInteraction.scala
@@ -3,16 +3,32 @@ package com.galales.tictactoe.services.interaction
 import com.galales.tictactoe.models.Move
 import com.galales.tictactoe.services.ui.UserInterfaceService
 
+import scala.util.{Failure, Success, Try}
+
 object ConsoleInteraction extends InteractionService {
 
   override def makeMove(implicit userInterface: UserInterfaceService): Move = {
     userInterface.outputMessage("Make your move: \n")
 
-    userInterface.outputMessage("Row: ")
-    val row = scala.io.StdIn.readInt
-    userInterface.outputMessage("Column: ")
-    val column = scala.io.StdIn.readInt
+    val row = requestInput("Row: ") - 1
+    val column = requestInput("Column: ") - 1
 
     Move(row, column)
+  }
+
+  private def requestInput(message: String)(implicit userInterface: UserInterfaceService): Int = {
+    userInterface.outputMessage(message)
+    Try(scala.io.StdIn.readInt) match {
+      case Success(i) =>
+        if(i >= 1 && i <= 3 )
+          i
+        else {
+          userInterface.outputMessage("The value must be 1, 2 or 3\n")
+          requestInput(message)
+        }
+      case Failure(_) =>
+        userInterface.outputMessage("The input is not a number, please try again\n")
+        requestInput(message)
+    }
   }
 }
