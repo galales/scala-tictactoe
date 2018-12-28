94109ab562252cb17939b3a4c0999275d430fe09
Alessio <alessio@freedamedia.com>
2018-12-28 21:15:39 +0100

    Improve test syntax

--- src/test/scala/com/galales/tictactoe/models/BoardSpec.scala
+++ src/test/scala/com/galales/tictactoe/models/BoardSpec.scala
@@ -7,39 +7,33 @@ class BoardSpec extends UnitSpec {
   "A Board with the default size (3)" should "be created and filled with None value" in {
     val board = Board()
     val expectedBoardSize = 3
-    assert(board.size == expectedBoardSize)
+
+    board.size should equal (expectedBoardSize)
 
     for(
       row <- 0 until expectedBoardSize;
       column <- 0 until expectedBoardSize
-    ) yield assert(board(row)(column).isEmpty)
+    ) yield board(row)(column) shouldBe empty
   }
 
   "A Board" should "be created with custom size 5 and filled with None value" in {
     val expectedBoardSize = 5
     val board = Board(expectedBoardSize)
-    assert(board.size == expectedBoardSize)
+
+    board.size should equal (expectedBoardSize)
 
     for(
       row <- 0 until expectedBoardSize;
       column <- 0 until expectedBoardSize
-    ) yield assert(board(row)(column).isEmpty)
+    ) yield board(row)(column) shouldBe empty
   }
 
   "An IndexOutOfBoundsException" should "thrown when trying to access elements outside size" in {
     val board = Board()
 
-    assertThrows[IndexOutOfBoundsException] {
-      board(0)(3)
-    }
-
-    assertThrows[IndexOutOfBoundsException] {
-      board(3)(0)
-    }
-
-    assertThrows[IndexOutOfBoundsException] {
-      board(3)(3)
-    }
+    an [IndexOutOfBoundsException] should be thrownBy board(0)(3)
+    an [IndexOutOfBoundsException] should be thrownBy board(3)(0)
+    an [IndexOutOfBoundsException] should be thrownBy board(3)(3)
   }
 
   "A Move" should "be applied returning GoodMove if correct, not applied and return BadMove otherwise" in {
