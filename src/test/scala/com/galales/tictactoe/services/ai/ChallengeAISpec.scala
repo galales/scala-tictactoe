f3d3f288b7bec1df6fc5c5bbad2f8abf0d4be3fb
Alessio <alessio@freedamedia.com>
2018-12-28 23:21:23 +0100

    ChallengeAISpec completed

---
+++ src/test/scala/com/galales/tictactoe/services/ai/ChallengeAISpec.scala
@@ -0,0 +1,100 @@
+package com.galales.tictactoe.services.ai
+
+import com.galales.tictactoe.UnitSpec
+import com.galales.tictactoe.models.{Board, Move, Player}
+
+class ChallengeAISpec extends UnitSpec {
+
+  "The Challenger" should "stay inside the playing field" in {
+    val board = Board()
+    val move = ChallengeAI.makeMove(board)
+
+    move.row should be >= 0
+    move.row should be < 3
+
+    move.column should be >= 0
+    move.column should be < 3
+  }
+
+  it should "try to win" in {
+    val board = Board()
+
+    // Row almost complete
+    board.execMove(Move(1, 0), Player.ai)
+    board.execMove(Move(1, 1), Player.ai)
+
+    val move = ChallengeAI.makeMove(board)
+
+    move.row should be (1)
+    move.column should be (2)
+  }
+
+  it should "with all" in {
+    val board = Board()
+
+    // Column almost complete
+    board.execMove(Move(0, 1), Player.ai)
+    board.execMove(Move(1, 1), Player.ai)
+
+    val move = ChallengeAI.makeMove(board)
+
+    move.row should be (2)
+    move.column should be (1)
+  }
+
+  it should "it's might" in {
+    val boardMain = Board()
+    val boardSecondary = Board()
+
+    // Main Diagonal almost complete
+    boardMain.execMove(Move(0, 0), Player.ai)
+    boardMain.execMove(Move(1, 1), Player.ai)
+
+    // Secondary Diagonal almost complete
+    boardSecondary.execMove(Move(0, 2), Player.ai)
+    boardSecondary.execMove(Move(1, 1), Player.ai)
+
+    val moveMain = ChallengeAI.makeMove(boardMain)
+    val moveSecondary = ChallengeAI.makeMove(boardSecondary)
+
+    moveMain.row should be (2)
+    moveMain.column should be (2)
+
+    moveSecondary.row should be (2)
+    moveSecondary.column should be (0)
+  }
+
+  it should "block the opponent" in {
+    val board = Board()
+
+    // User is winning
+    board.execMove(Move(0, 1), Player.user)
+    board.execMove(Move(1, 1), Player.user)
+
+    // AI has nothing in his hands
+    board.execMove(Move(2, 2), Player.ai)
+
+    val move = ChallengeAI.makeMove(board)
+
+    move.row should be (2)
+    move.column should be (1)
+  }
+
+  it should "unless the victory is in its pocket" in {
+    val board = Board()
+
+    // User is winning
+    board.execMove(Move(0, 1), Player.user)
+    board.execMove(Move(1, 1), Player.user)
+
+    // And AI too
+    board.execMove(Move(0, 2), Player.ai)
+    board.execMove(Move(1, 2), Player.ai)
+
+    val move = ChallengeAI.makeMove(board)
+
+    move.row should be (2)
+    move.column should be (2)
+  }
+
+}
