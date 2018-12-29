c05be7039bbee66fc5cfa6e32d96eb21bc069a0e
Alessio <alessio@freedamedia.com>
2018-12-29 18:38:50 +0100

    SimpleAI add missing test for no winning moves left

--- src/test/scala/com/galales/tictactoe/services/ai/SimpleAISpec.scala
+++ src/test/scala/com/galales/tictactoe/services/ai/SimpleAISpec.scala
@@ -5,7 +5,7 @@ import com.galales.tictactoe.models.{Board, Move, Player}
 
 class SimpleAISpec extends UnitSpec {
 
-  "A Simple kind of Man" should "come sit beside me" in {
+  "A Simple Man" should "come sit beside me" in {
     val board = Board()
     val move = SimpleAI.makeMove(board)
 
@@ -62,6 +62,27 @@ class SimpleAISpec extends UnitSpec {
 
     moveSecondary.row should be (2)
     moveSecondary.column should be (0)
+  }
+
+  it should "not worry, he'll find himself" in {
+    val board = Board()
+
+    // No possible winning moves left
+    board.execMove(Move(0, 0), Player.user)
+    board.execMove(Move(1, 1), Player.user)
+    board.execMove(Move(2, 2), Player.user)
+
+    val move = SimpleAI.makeMove(board)
+
+    move.row should be >= 0
+    move.row should be < 3
+
+    move.column should be >= 0
+    move.column should be < 3
+
+    move should not be Move(0, 0)
+    move should not be Move(1, 1)
+    move should not be Move(2, 2)
   }
 
 }
