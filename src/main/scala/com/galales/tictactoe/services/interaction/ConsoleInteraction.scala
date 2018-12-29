693534d5431685162452dfc41b2589f65b55006b
Alessio <alessio@freedamedia.com>
2018-12-29 18:22:30 +0100

    Refactor Interaction trait and classes to make tests easier and to delegate to classes only the user input

--- src/main/scala/com/galales/tictactoe/services/interaction/ConsoleInteraction.scala
+++ src/main/scala/com/galales/tictactoe/services/interaction/ConsoleInteraction.scala
@@ -1,34 +1,15 @@
 package com.galales.tictactoe.services.interaction
 
-import com.galales.tictactoe.models.Move
-import com.galales.tictactoe.services.ui.UserInterfaceService
-
 import scala.util.{Failure, Success, Try}
 
 object ConsoleInteraction extends InteractionService {
 
-  override def makeMove(boardSize: Int)(implicit userInterface: UserInterfaceService): Move = {
-    userInterface.outputMessage("Make your move: \n")
-
-    val row = requestInput("Row: ", boardSize) - 1
-    val column = requestInput("Column: ", boardSize) - 1
-
-    Move(row, column)
-  }
-
-  private def requestInput(message: String, boardSize: Int)(implicit userInterface: UserInterfaceService): Int = {
-    userInterface.outputMessage(message)
+  override def requestInput: Either[String, Int] = {
     Try(scala.io.StdIn.readInt) match {
       case Success(i) =>
-        if(i >= 1 && i <= boardSize )
-          i
-        else {
-          userInterface.outputErrorMessage(s"The value must be between 1 and $boardSize\n")
-          requestInput(message, boardSize)
-        }
+        Right(i)
       case Failure(_) =>
-        userInterface.outputErrorMessage("The input is not a number, please try again\n")
-        requestInput(message, boardSize)
+        Left("The input is not a number, please try again\n")
     }
   }
 }
--- src/main/scala/com/galales/tictactoe/services/interaction/InteractionService.scala
+++ src/main/scala/com/galales/tictactoe/services/interaction/InteractionService.scala
@@ -4,5 +4,34 @@ import com.galales.tictactoe.models.Move
 import com.galales.tictactoe.services.ui.UserInterfaceService
 
 trait InteractionService {
-  def makeMove(boardSize: Int)(implicit userInterface: UserInterfaceService): Move
+
+  def requestInput: Either[String, Int]
+
+
+  def makeMove(boardSize: Int)(implicit userInterface: UserInterfaceService): Move = {
+    userInterface.outputMessage("Make your move: \n")
+
+    val row = getValue("Row: ", boardSize) - 1
+    val column = getValue("Column: ", boardSize) - 1
+
+    Move(row, column)
+  }
+
+  private def getValue(message: String, boardSize: Int)(implicit userInterface: UserInterfaceService): Int = {
+    userInterface.outputMessage(message)
+
+    requestInput match {
+      case Left(s) =>
+        userInterface.outputErrorMessage(s)
+        getValue(message, boardSize)
+      case Right(i) =>
+        if(i >= 1 && i <= boardSize )
+          i
+        else {
+          userInterface.outputErrorMessage(s"The value must be between 1 and $boardSize\n")
+          getValue(message, boardSize)
+        }
+    }
+
+  }
 }
